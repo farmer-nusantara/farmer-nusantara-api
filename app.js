7445433ad4c0ee500ab4743b0462d7cc345a600e
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-05-03 09:00:20 +0700

    remove ejs configuration

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var app = express();
 const mongoose = require('mongoose');
@@ -18,8 +17,7 @@ mongoose.connect(NODE_ENV !== "development" ? MONGO_PROD_URI : MONGO_DEV_URI, {
     .catch(err => console.log(err));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(logger('dev'));
@@ -28,7 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
