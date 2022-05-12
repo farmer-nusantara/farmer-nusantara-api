fccdad77c68e8563380b0aa7dbfaaf67bc96a8d1
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-05-12 07:45:55 +0700

    changed method http request

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 router.post('/auth/signup', userController.validates('signUp'), userController.signUp);
 router.post('/auth/email-token-activation', userController.validates('sendTokenActivationAccount'), userController.sendTokenActivationAccount);
-router.post('/auth/status-account', userController.changeStatusAccount);
+router.put('/auth/status-account', userController.changeStatusAccount);
 router.post('/auth/signin', userController.validates('signIn'), userController.signIn);
 router.post('/auth/email-token-reset', userController.validates('sendTokenActivationAccount'), userController.sendCodeResetPassword);
 router.post('/auth/check-token-reset', userController.checkSecretCodeforResetPassword);
-router.post('/auth/change-password', userController.validates('resetPassword'), userController.changePasswordAccount)
+router.put('/auth/change-password', userController.validates('resetPassword'), userController.changePasswordAccount)
 router.get('/auth/user/:userId', auth, userController.getDetailUser);
 
 module.exports = router;
