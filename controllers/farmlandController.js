70fdd1f71785be2455eaf4a086c009f247b4780c
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-05-16 22:07:07 +0700

    created farmland and upload cover of farmland api

--- controllers/farmlandController.js
+++ controllers/farmlandController.js
@@ -1,5 +1,23 @@
 const farmlandModel = require('../models/farmlandModel');
 const { check } = require('express-validator');
+const multer = require('multer');
+const util = require('util');
+const path = require('path');
+
+function checkFileType(file, cb) {
+	// Allowed ext
+	const fileTypes = /jpeg|jpg|png|gif/;
+	// Check ext
+	const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+	// Check mime
+	const mimeType = fileTypes.test(file.mimetype);
+
+	if (mimeType && extName) {
+		return cb(null, true);
+	} else {
+		cb("Error: Images Only !!!");
+	}
+}
 
 module.exports = {
   createFarmland: async (req, res, next) => {
@@ -16,6 +34,43 @@ module.exports = {
       });
 
       return res.status(201).json(farmland);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  },
+  uploadFarmCover: async (req, res, next) => {
+    const { userId } = req.params;
+
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, "temp/");
+      },
+      filename: (req, file, cb) => {
+        cb(null, userId + '-' + Date.now() + path.extname(file.originalname).toLowerCase());
+      },
+    });
+
+    const upload = multer({
+      storage: storage,
+      limits: { fileSize: 2000000 },
+      fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+      },
+    }).single("image");
+
+    let uploadFile = util.promisify(upload);
+
+    try {
+      await uploadFile(req, res);
+      if (req.file == undefined) {
+        return res.status(400).send({ message: "Please upload a file!" });
+      }
+
+      res.status(200).json({
+        message: "Uploaded the file successfully",
+        filename: req.file.filename,
+      });
+
     } catch (error) {
       return res.status(400).json({ message: error.message });
     }
--- routes/api.js
+++ routes/api.js
@@ -14,5 +14,6 @@ router.put('/auth/change-password', userController.validates('resetPassword'), u
 router.get('/auth/user/:userId', auth, userController.getDetailUser);
 
 router.post('/farmland', auth, farmlandController.validates('createFamland'), farmlandController.createFarmland);
+router.post('/farm-cover/uploads/:userId', auth, farmlandController.uploadFarmCover);
 
 module.exports = router;
